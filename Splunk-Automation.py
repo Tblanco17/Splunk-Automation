75d2fd8393907a71b4087cd0e27a84195e8bee37
Tyler Blanco <42285917+Tblanco17@users.noreply.github.com>
2019-03-19 19:46:02 -0400

    Update Splunk-Automation.py

--- Splunk-Automation.py
+++ Splunk-Automation.py
@@ -1,6 +1,6 @@
 import os
 
-os.system("sudo wget wget -O splunk-7.2.5-088f49762779-Linux-x86_64.tgz 'https:$
+os.system("sudo wget -O splunk-7.2.5-088f49762779-Linux-x86_64.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.2.5&product=splunk&filename=splunk-7.2.5-088f49762779-Linux-x86_64.tgz&wget=true'"
 os.system("sudo tar -xvf splunk-7.2.5-088f49762779-Linux-x86_64.tgz -C /opt")
 os.system("/opt/splunk/bin/splunk start --accept-license")
 os.system("Tblanco17")
